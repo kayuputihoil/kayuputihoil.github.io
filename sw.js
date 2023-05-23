213ce0cd6a27ad7ddd0d55f64f45abc7c5b5412e
kayuputihoil <kayuputihoil.ambon@gmail.com>
2023-05-23 17:21:58 +0900

    NEW commit

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@ workbox.setConfig({
   debug: true, // Aktifkan mode debug untuk pengembangan
 });
 
-workbox.core.setCacheNameDetails({prefix:'workbox',suffix:'v5'})//jika ubah suffix jgn lupa ubah di suffix di event activate 
+workbox.core.setCacheNameDetails({prefix:'workbox',suffix:'v7'})//jika ubah suffix jgn lupa ubah di suffix di event activate 
 
 workbox.precaching.precacheAndRoute([
   // Daftar aset yang ingin Anda cache dan perbarui secara otomatis
@@ -48,7 +48,7 @@ self.addEventListener('activate', (event) => {
         keyList.filter(key => {
           // return true;
           // return !workbox.core.keyList.includes(key);
-          return !key.startsWith('workbox') || !key.endsWith('v5');
+          return !key.startsWith('workbox') || !key.endsWith('v7');
         }).map(key => {
             console.log(key);
             return caches.delete(key);
