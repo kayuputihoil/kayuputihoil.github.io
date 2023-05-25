596a1e6c2934e7203fb4074e41da27eb9c10ceec
kayuputihoil <kayuputihoil.ambon@gmail.com>
2023-05-26 02:21:51 +0900

    new file origin63

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.5.4/workbox-sw.js');
 
 workbox.setConfig({
   debug: true, // Aktifkan mode debug untuk pengembangan
 });
 
-workbox.core.setCacheNameDetails({prefix:'workbox',suffix:'v3'})//jika ubah suffix jgn lupa ubah di suffix di event activate 
+workbox.core.setCacheNameDetails({prefix:'workbox',suffix:'v1'})//jika ubah suffix jgn lupa ubah di suffix di event activate 
 
 // Strategi runtime caching untuk permintaan yang cocok dengan kondisi tertentu
 workbox.routing.registerRoute(
@@ -25,7 +25,7 @@ workbox.routing.registerRoute(
         purgeOnQuotaError: true, // Menghapus entri-cache jika terjadi kesalahan kuota
       })
     ]
-  //   cacheName: 'workbox-runtime-v3', // Nama cache untuk file CSS
+  //   cacheName: 'workbox-runtime-v1', // Nama cache untuk file CSS
   // plugins: [
   //   new workbox.expiration.Plugin({
   //     maxAgeSeconds: 120,
@@ -91,7 +91,7 @@ self.addEventListener('activate', (event) => {
         keyList.filter(key => {
           // return true;
           // return !workbox.core.keyList.includes(key);
-          return !key.startsWith('workbox') || !key.endsWith('v3');
+          return !key.startsWith('workbox') || !key.endsWith('v1');
         }).map(key => {
             console.log(key);
             return caches.delete(key);
