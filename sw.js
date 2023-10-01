30cafe239e4b140fb58f2dd9ab7733c8b8bbac1b
kayuputihoil <kayuputihoil.ambon@gmail.com>
2023-10-02 08:02:41 +0900

    new file

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ workbox.setConfig({
   debug: false, // Aktifkan mode debug untuk pengembangan
 });
 
-workbox.core.setCacheNameDetails({prefix:'workbox',suffix:'v9'})//jika ubah suffix jgn lupa ubah di suffix di event activate 
+workbox.core.setCacheNameDetails({prefix:'workbox',suffix:'v7'})//jika ubah suffix jgn lupa ubah di suffix di event activate 
 
 // Set cache expiration time (e.g., 7 days)
 const cacheExpiration = 7 * 24 * 60 * 60; // in seconds
@@ -33,7 +33,7 @@ workbox.routing.registerRoute(
       //   purgeOnQuotaError: true, // Menghapus entri-cache jika terjadi kesalahan kuota
       // })
     ]
-  //   cacheName: 'workbox-runtime-v9', // Nama cache untuk file CSS
+  //   cacheName: 'workbox-runtime-v7', // Nama cache untuk file CSS
   // plugins: [
   //   new workbox.expiration.Plugin({
   //     maxAgeSeconds: 120,
@@ -124,7 +124,7 @@ self.addEventListener('activate', (event) => {
         keyList.filter(key => {
           // return true;
           // return !workbox.core.keyList.includes(key);
-          return !key.startsWith('workbox') || !key.endsWith('v9');
+          return !key.startsWith('workbox') || !key.endsWith('v7');
         }).map(key => {
             console.log(key);
             return caches.delete(key);
